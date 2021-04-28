be5c0c59930d9b98b8ab4334ac8b178c3b09c135
igoreta <ivoronline@gmail.com>
2021-04-28 15:03:10 +0200

    Initial commit

---
+++ src/test/java/com/ivoronline/springboot_soap/SpringbootSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSoapApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
